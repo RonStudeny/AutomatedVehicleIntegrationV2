572371f84c147aa8499c3b18d6cc163b967797e8
Ron Studený <ronstudeny@gmail.com>
2022-03-14 15:18:35 +0100

    Car basics

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedVehicleIntegrationV2
+{
+    public class Car
+    {
+        public double SpeedMs { get; set; }
+        public double SpeedKmh { get; set; } // for user only
+        public double RouteLength { get; set; }
+        public double RouteProgress { get; set; }
+        public bool LightState { get; set; }
+        public RoadTypes RoadType { get; set; }
+
+        public enum RoadTypes { Normal, Highway, Tunnel, Bridge };
+
+    }
+}
