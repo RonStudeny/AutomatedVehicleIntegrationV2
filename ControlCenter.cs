787479d1635e662ba3f6eb6bb03363d95d69ec99
Ron Studený <ronstudeny@gmail.com>
2022-03-15 00:01:05 +0100

    Class prep

---
+++ ControlCenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedVehicleIntegrationV2
+{
+    public delegate void GlobalTickHandler();
+
+    public class ControlCenter
+    {
+        public static List<Car> fullCarList;
+        
+    }
+}
---
+++ MainTimer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedVehicleIntegrationV2
+{
+
+    public class MainTimer
+    {
+        public event GlobalTickHandler GlobalTick;
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        public MainTimer()
+        {
+            timer.Tick += new EventHandler(TimerTick);
+        }
+
+        private void TimerTick(object sender, EventArgs e) => GlobalTick();
+
+    }
+}
